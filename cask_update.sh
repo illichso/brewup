2a9d76164a54e3326d10132f50deb2a9667ff5cc
illichso <illichso@gmail.com>
2016-12-26 22:16:42 +0100

    move cask one by one unpdate to separate file

---
+++ cask_update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+$lineBreak
+casks=( $(brew cask list) );
+for cask in ${casks[@]}
+do
+    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
+    installed=$(find "/usr/local/Caskroom/$cask" -type d -mindepth 1 -maxdepth 1 -name "$version")
+
+    if [[ -z $installed ]]; then
+        echo "${red}${cask}${reset} requires ${red}update${reset}."
+        (set -x; brew cask uninstall $cask --force;)
+        (set -x; brew cask install $cask --force;)
+    else
+        echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done;
